383fbfbd30ab7d87bd3c7c5b9fe3375029961ec1
ilabrosimov <ilabrosimov@gmail.com>
2021-06-05 19:29:50 +0300

    Старт перед д/з номер 6

--- ColorSlider/ViewController.swift
+++ ColorSlider/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class ViewController: UIViewController {
     
     //MARK: - IB Outlets
-    @IBOutlet weak var mainColorView: UIView!
+    @IBOutlet weak var colorView: UIView!
     
     @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
@@ -23,23 +23,8 @@ class ViewController: UIViewController {
     //MARK: - Override Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        mainColorView.layer.cornerRadius = 15
+        startUI()
         
-        redSlider.tintColor = .red
-        greenSlider.tintColor = .green
-        blueSlider.tintColor = .blue
-        
-        redSlider.minimumValue = 0
-        greenSlider.minimumValue = 0
-        blueSlider.minimumValue = 0
-        
-        redSlider.maximumValue = 1
-        greenSlider.maximumValue = 1
-        blueSlider.maximumValue = 1
-        
-        redSlider.value = 0
-        greenSlider.value = 0
-        blueSlider.value = 0
     }
     
     //MARK: - IB Actions
@@ -52,7 +37,21 @@ class ViewController: UIViewController {
         greenValue.text = String(NSString(format: "%.2f", greenColor))
         blueValue.text = String(NSString(format: "%.2f", blueColor ))
         
-        mainColorView.backgroundColor = UIColor.init(red: redColor, green: greenColor, blue: blueColor, alpha: 1)
+        colorView.backgroundColor = UIColor.init(red: redColor, green: greenColor, blue: blueColor, alpha: 1)
     }
+    
+    private  func startUI () {
+        colorView.layer.cornerRadius = 15
+        
+        redSlider.tintColor = .red
+        greenSlider.tintColor = .green
+
+        redSlider.value = 0
+        greenSlider.value = 0
+        blueSlider.value = 0
+    }
+    
 }
+
+
 
